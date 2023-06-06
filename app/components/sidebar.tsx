b2f8b63d5b2577f00813fac1491fc41af9907a15
aerocaterpillar <volgen@gmail.com>
2023-06-06 10:47:09 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>TRGPT ver.2 GPT4</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Powered by Digital Team, FOR INTERNAL TEST ONLY, DO NOT UPLOAD SENSITIVE INFO.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
